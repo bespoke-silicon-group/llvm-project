ab0d39920e98b13a83d76858eb91a9b82326035e
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-06-03 01:52:46 -0700

    File header

--- llvm/lib/Target/RISCV/VanillaPasses.cpp
+++ llvm/lib/Target/RISCV/VanillaPasses.cpp
@@ -1,3 +1,15 @@
+//===-- VanillaPasses.cpp - Define TargetMachine for RISCV -----------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Implements Vanilla Core specific passes.
+//
+//===----------------------------------------------------------------------===//
+
 #include "RISCV.h"
 #include "llvm/Pass.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
@@ -5,6 +17,8 @@
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
+
+/// Example Machine Function Pass for Vanilla Core
 
 namespace {
 
@@ -14,6 +28,7 @@ struct VanillaPass : public MachineFunctionPass {
   VanillaPass(): MachineFunctionPass(ID) {}
 
   bool runOnMachineFunction(MachineFunction &MF) override {
+    // Do nothing
     return false;
   }
 };
