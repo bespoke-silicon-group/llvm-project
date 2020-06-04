9755b329337d2cba1805af88f0f81e27f7e5288e
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-06-04 01:27:37 -0700

    Functional custom scheduler policy

--- llvm/lib/Target/RISCV/VanillaPasses.cpp
+++ llvm/lib/Target/RISCV/VanillaPasses.cpp
@@ -11,11 +11,13 @@
 //===----------------------------------------------------------------------===//
 
 #include "RISCV.h"
+#include "RISCVInstrInfo.h"
 #include "VanillaPasses.h"
 #include "llvm/Pass.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineFunction.h"
 #include "llvm/CodeGen/MachineScheduler.h"
+#include "llvm/CodeGen/MachineMemOperand.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
@@ -46,24 +48,50 @@ char VanillaPass::ID = 0;
 
 /// Vanilla Core Scheduler
 SUnit *VanillaScheduler::pickNode (bool &IsTopNode) {
-  SUnit* SU = Top.pickOnlyChoice(); 
-
-  //if (!SU) {
-  //  for(SU : Top.Available) {
-  //    if (!canIssueNow()) continue;
-  //    
-  //    
-  //    a
-  //  }
-
-  if (SU) {
-    LLVM_DEBUG(dbgs() << "Vanilla Scheduling SU(" << SU->NodeNum << ") "
-                      << *SU->getInstr());
-    Top.removeReady(SU);
-    IsTopNode = true;
-    return SU;
+  if (DAG->top() == DAG->bottom()) {
+    assert(Top.Available.empty() && Top.Pending.empty() &&
+           Bot.Available.empty() && Bot.Pending.empty() && "ReadyQ garbage");
+    return nullptr;
   }
 
-  // Fallback to generic scheduler
-  return GenericScheduler::pickNode(IsTopNode);
+  SUnit *SU;
+
+  do {
+    SU = Top.pickOnlyChoice();
+
+    // Priorotize loads from address space 1
+    if (!SU) {
+      for(SUnit* SUi : Top.Available) {
+        MachineInstr* MI = SUi->getInstr();
+        ArrayRef<MachineMemOperand*> memops = MI->memoperands();
+        if (MI->mayLoad() &&
+            !memops.empty() && memops[0]->getAddrSpace() == 1) {
+          LLVM_DEBUG(dbgs() << "VanillaScheduler bumping load ("
+                            << SU->NodeNum << ") "
+                            << *MI);
+          SU = SUi;
+          break;
+        }
+      }
+    }
+
+    // Fallback to top-down policy of generic scheduler
+    if (!SU) {
+      CandPolicy NoPolicy;
+      TopCand.reset(NoPolicy);
+      pickNodeFromQueue(Top, NoPolicy, DAG->getTopRPTracker(), TopCand);
+      assert(TopCand.Reason != NoCand && "failed to find a candidate");
+      SU = TopCand.SU;
+    }
+    IsTopNode = true;
+  } while (SU->isScheduled);
+
+  if (SU->isTopReady())
+    Top.removeReady(SU);
+  if (SU->isBottomReady())
+    Bot.removeReady(SU);
+
+  LLVM_DEBUG(dbgs() << "Scheduling SU(" << SU->NodeNum << ") "
+                    << *SU->getInstr());
+  return SU;
 }
